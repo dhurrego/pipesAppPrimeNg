3a2c8c18c80db1db021f6709c9c42127085cb416
Deivid Urrego <deivid_199615@hotmail.com>
2021-04-06 10:00:11 -0500

    Otros ejemplos con i18nSelectPipe y i18nPluralPipe

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -18,10 +18,28 @@ export class NoComunesComponent {
   }
 
   //i18nPlural
-  clientes: string[] = ['Deivid','Alejandra'];
+  clientes: string[] = ['Deivid','Alejandra','Samantha','Juan David','Leydi','Ines'];
   clientesMapa = {
     '=0': 'no tenemos ningún cliente esperando',
     '=1': 'tenemos 1 cliente esperando',
     'other': 'tenemos # clientes esperando'
   }
+
+  cambiarPersona(){
+
+    if(this.nombre == 'Deivid'){
+      this.nombre = 'Alejandra';
+      this.genero = 'femenino';
+    }else{
+      this.nombre = 'Deivid';
+      this.genero = 'masculino';
+    }
+    
+  }
+
+  borrarCliente(){
+    this.clientes.shift(); // Elimina el primer elemento del array
+    //this.clientes.pop(); // Elimina el ultimo elemento del array
+  }
+
 }
